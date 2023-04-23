dd93b60ba8d4144b796276d151241b5390ae24a7
Your Name <ardnatan@gmail.com>
2023-04-23 19:35:39 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let profilePic = document.getElementById("profile-pic");
+let inputFile = document.getElementById("input-file");
+
+inputFile.onchange = function(){
+    profilePic.src = URL.createObjectURL(inputFile.files[0]);
+}
